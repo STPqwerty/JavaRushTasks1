85ff53b7e4b9d71d57bfd5a15c3ade14b0cfd356
alex <akuzin20@gmail.com>
2018-11-27 12:05:34 +0300

    2

--- 4.JavaCollections/src/com/javarush/task/task33/task3310/HashMapStorageStrategy.java
+++ 4.JavaCollections/src/com/javarush/task/task33/task3310/HashMapStorageStrategy.java
@@ -3,32 +3,41 @@ package com.javarush.task.task33.task3310;
 import com.javarush.task.task33.task3310.strategy.StorageStrategy;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class HashMapStorageStrategy implements StorageStrategy {
-    HashMap<Long, String> data;
+    private HashMap<Long, String> data = new HashMap<>();
 
     @Override
     public boolean containsKey(Long key) {
-        return false;
+        return data.containsKey(key);
     }
 
     @Override
     public boolean containsValue(String value) {
-        return false;
+        return data.containsValue(value);
     }
 
     @Override
     public void put(Long key, String value) {
-
+        data.put(key, value);
     }
 
     @Override
     public Long getKey(String value) {
+        if(value == null || value.isEmpty()){
+            throw new IllegalArgumentException();
+        }
+        for(Map.Entry<Long, String> pair : data.entrySet()){
+            if(value.equals(pair.getValue())){
+                return pair.getKey();
+            }
+        }
         return null;
     }
 
     @Override
     public String getValue(Long key) {
-        return null;
+        return data.get(key);
     }
 }
